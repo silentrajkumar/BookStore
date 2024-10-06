d6c596e74bb5d3f67f39b3d4ddfe4db591aaaba3
silentrajkumar <rajkumar.ramu21@gmail.com>
2024-10-06 18:24:40 +0530

    latest change

--- src/main/java/com/crni99/bookstore/BookStoreApplication.java
+++ src/main/java/com/crni99/bookstore/BookStoreApplication.java
@@ -8,8 +8,8 @@ public class BookStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
-		System.out.println("Hi");
-		System.out.println("Hello");
+		System.out.println("Hi ");
+		System.out.println("Hello12");
 	}
 
 }
