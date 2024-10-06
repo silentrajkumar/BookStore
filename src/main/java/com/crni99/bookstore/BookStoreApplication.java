ba62af8c344cb30b8ad9c25214ddbf1dc847526b
silentrajkumar <rajkumar.ramu21@gmail.com>
2024-10-06 18:07:44 +0530

    add

--- src/main/java/com/crni99/bookstore/BookStoreApplication.java
+++ src/main/java/com/crni99/bookstore/BookStoreApplication.java
@@ -9,6 +9,7 @@ public class BookStoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
 		System.out.println("Hi");
+		System.out.println("Hello");
 	}
 
 }
