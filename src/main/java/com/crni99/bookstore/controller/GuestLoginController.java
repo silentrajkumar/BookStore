2fe556e91e446ba9f435dda15b1bb94310a42733
silentrajkumar <rajkumar.ramu21@gmail.com>
2024-10-08 23:41:36 +0530

    Changes in controller and add conditions

--- src/main/java/com/crni99/bookstore/controller/GuestLoginController.java
+++ src/main/java/com/crni99/bookstore/controller/GuestLoginController.java
@@ -19,38 +19,37 @@ import com.crni99.bookstore.model.UserDetail;
 import com.crni99.bookstore.service.GuesLoginService;
 import com.fasterxml.jackson.annotation.JacksonInject.Value;
 
-
-
 @Controller
 public class GuestLoginController {
-	
+
 	@Autowired
 	private GuesLoginService gueslogin;
-	
-	
-  @RequestMapping(value="/guestLogin", method = RequestMethod.GET)
-  public  String showGuestPage() {
-		/*Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {// PEN-2 - Create new workflow in login controller by N787876
-//			if(userService.equals(userService))
-*///			{
+
+	@RequestMapping(value = "/guestLogin", method = RequestMethod.GET)
+	public String showGuestPage() {
+		/*
+		 * Authentication authentication =
+		 * SecurityContextHolder.getContext().getAuthentication(); if (authentication ==
+		 * null || authentication instanceof AnonymousAuthenticationToken) {// PEN-2 -
+		 * Create new workflow in login controller by N787876 //
+		 * if(userService.equals(userService))
+		 */// {
 //				System.out.println("Hi user"+u.getUsername());
 //				System.out.println("Hi user"+u.getPassword());
 //			}
 //			else {
 //				System.out.println("you are a wrong user");
 //			}
-			return "guestLogin";
-			
-			
-			/*
-			 * } return "redirect:/cart";
-			 */
-		
+		return "guestLogin";
+
+		/*
+		 * } return "redirect:/cart";
+		 */
+
 	}
- 
-  @RequestMapping(value="/guestLoginForm", method = RequestMethod.POST)
-  public  String showGuestPage1(UserDetail u) {
+
+	@RequestMapping(value = "/guestLoginForm", method = RequestMethod.POST)
+	public String showGuestPage1(UserDetail userInput) {
 
 //	  if(u.getUsername().equalsIgnoreCase("rajkumar") && u.getPassword().equals("abc")) {
 //		  System.out.print("user");}
@@ -58,28 +57,84 @@ public class GuestLoginController {
 //	  {
 //		  System.out.print("you are wrong user");
 //	  }
-	  boolean flag=false;
-	List <UserDetail> userLogin=gueslogin.getUserSer();
-	for (UserDetail userDetail : userLogin) {
-		
-		if(userDetail.getUsername().equalsIgnoreCase(u.getUsername())&& userDetail.getPassword().equals(u.getPassword())) {
-			flag=true;
+		//boolean flag = false;
+		int flag=0;
+		List<UserDetail> userLogin = gueslogin.getUserSer();
+		for (UserDetail userDetail : userLogin) {
+
+//		 if(userDetail.getUsername().equalsIgnoreCase(u.getUsername())&& userDetail.getPassword().equals(u.getPassword())) {
+//			flag=true;
+//		}
+//	}
+//		if(flag) {
+//			System.out.println("Hi user "+u.getUsername());
+//		}
+//		else {
+//			System.out.println("Wrong user");
+//		}
+//		if(userDetail.getUsername().equals("Nivedha")&& userDetail.getPassword().equals("mn0022")) {
+//			flag=true;
+//		}
+//		else if(userDetail.getUsername().equals(u) && userDetail.getPassword().equals(u)) {
+//			System.out.println("Hi user"+u.getUsername());
+//		}
+//		}
+//		
+//		if(flag) {
+//			System.out.println("Hi admin "+u.getUsername());
+//		}
+//		else {
+//			System.out.println("Hi user");
+//		}
+			if(userInput.getUsername().equalsIgnoreCase("Nivedha")&& (userInput.getPassword().equals("mn0022"))){
+				flag=1;
+				//System.out.println("Hi Admin"+ userDetail.getUsername());
+				break;
+			}
+			else if(userInput.getUsername().equalsIgnoreCase("Vinoth")&&(userInput.getPassword().equals("md22"))) {
+				flag=2;
+				break;
+			}
+			else if(userInput.getUsername().equalsIgnoreCase(userDetail.getUsername())&&userInput.getPassword().equals(userDetail.getPassword())&& userInput.getUsername().startsWith("ab")) {
+				flag=3;
+				//System.out.println("NOrml user");
+				break;
+			}
+			else if(userInput.getUsername().equalsIgnoreCase(userDetail.getUsername())&&userInput.getPassword().equals(userDetail.getPassword())) {
+				flag=4;
+			}
+			
+//			else {
+//				System.out.println("Nouser found ");
+//			}
+			
+			}
+		if(flag==1) {
+			System.out.println("Hi Admin, "+userInput.getUsername().toUpperCase()+" Password : "+userInput.getPassword());
 		}
-	}
-		if(flag) {
-			System.out.println("Hi user"+u.getUsername());
+		else if(flag==2) {
+			System.out.println("HI Super User "+userInput.getUsername().toUpperCase()+" Password : "+userInput.getPassword());
+		}
+		
+		else if(flag==3 ) {
+			System.out.println("HI  Real User "+userInput.getUsername().toUpperCase()+" Password : "+userInput.getPassword());
+		}
+		else if(flag==4)
+		{
+			System.out.println("HI  Non-Real User "+userInput.getUsername().toUpperCase()+" Password : "+userInput.getPassword());
 		}
 		else {
-			System.out.println("Wrong user");
+			System.out.println("sorry!!Wrong user"+userInput.getUsername().toUpperCase());
 		}
-	
-	
-		return "forward:/cart";
+			
+			
 		
+		return "forward:/cart";
+
 	}
+
 	@GetMapping("/access-denied-page")
 	public String showAccessDeniedPage() {
 		return "error";
 	}
-  }
-
+}
--- src/main/java/com/crni99/bookstore/repository/GuestLoginDaoImpl.java
+++ src/main/java/com/crni99/bookstore/repository/GuestLoginDaoImpl.java
@@ -12,9 +12,12 @@ public class GuestLoginDaoImpl implements GuestLoginDao{
 
 	@Override
 	public List<UserDetail> getUserDao() {
-		List<UserDetail> user=Arrays.asList(new UserDetail("Nivedha","mn0022"),
-				new UserDetail("Vinoth","md22"),
-				new UserDetail("Reshma","jfff22")	);
+		List<UserDetail> user=Arrays.asList(new UserDetail("Nivedha","mn0022"),//aDMIN
+				new UserDetail("Vinoth","md22"),//hI USER
+				new UserDetail("Reshma","jfff22"),
+				new UserDetail("abu","97bhs"),
+				new UserDetail("abishek","97bhs")
+				);
 		return user;
 	}
 
